5d296504f123dc87cb0744b5571476a2c55cbaa8
unknown <navinpnchal@gmail.com>
2019-11-01 16:47:50 +0530

    Naveen Panchal

--- e_library/book/4.py
+++
@@ -1,9 +0,0 @@
-num=int(input("please enter the number:"))
-x = list(range(1,num+1))
-
-divisorList = []
-
-for number in x:
-    if num % number == 0:
-        divisorList.append(number)
-print(divisorList)
